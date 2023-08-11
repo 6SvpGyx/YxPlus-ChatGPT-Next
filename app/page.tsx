102e48a69cfa460358776081c0a0112f05a8ed40
yuxian <1606656170@qq.com>
2023-08-11 14:07:54 +0800

    add console

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,15 @@ import { Analytics } from "@vercel/analytics/react";
 import { Home } from "./components/home";
 
 import { getServerSideConfig } from "./config/server";
-import { useEffect } from "react";
 
 const serverConfig = getServerSideConfig();
 
 export default async function App() {
-  useEffect(() => {
-    window.console &&
-      console.log(
-        "%c技术交流欢迎联系".concat("\nQQ：1606656170", "\nWeChat: bbouo-y"),
-        "color: blue",
-      );
-  }, []);
-
+  window.console &&
+    console.log(
+      "%c技术交流欢迎联系".concat("\nQQ：1606656170", "\nWeChat: bbouo-y"),
+      "color: blue",
+    );
   return (
     <>
       <Home />
