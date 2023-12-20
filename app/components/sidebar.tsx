f215fec3a5a96bfde1ee0d1b9fc76c420660c9ad
yuxian <1606656170@qq.com>
2023-12-20 22:47:16 +0800

    update host url

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -153,7 +153,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          YxPlus ChatGPT
+          - Yu Xian
         </div>
         <div className={styles["sidebar-sub-title"]}>WeChat: abstrue</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
